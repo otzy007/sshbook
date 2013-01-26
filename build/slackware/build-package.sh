591e0549f8098c575715f6fc9f6f798a5146f2e2
Andrei Gliga <otzy_007@yahoo.com>
2013-01-26 22:12:15 +0200

    slackware packagin improvement

--- build/slackware/build-package.sh
+++ build/slackware/build-package.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 VERSION=0.1.2
-
-if [ -d /tmp/sshbook ]; then
-   rm -r /tmp/sshbook
+TEMPDIR=/tmp/sshbook-$VERSION
+if [ -d $TEMPDIR ]; then
+   rm -rf $TEMPDIR
 fi
 
-mkdir -p /tmp/sshbook/usr/bin
-mkdir /tmp/sshbook/install
-mkdir -p /tmp/sshbook/usr/share/doc/sshbook-$VERSION
+mkdir -p $TEMPDIR/usr/bin
+mkdir $TEMPDIR/install
+mkdir -p $TEMPDIR/usr/share/doc/sshbook-$VERSION
 
-cp ../../sshbook /tmp/sshbook/usr/bin
-cp slack-desc /tmp/sshbook/install
-cp ../../README.md /tmp/sshbook/usr/share/doc/sshbook-$VERSION
-cd /tmp/sshbook
+cp ../../sshbook $TEMPDIR/usr/bin
+cp slack-desc $TEMPDIR/install
+cp ../../README.md $TEMPDIR/usr/share/doc/sshbook-$VERSION
+cd $TEMPDIR
 
 /sbin/makepkg -l y -c n /tmp/sshbook-$VERSION-noarch-1oz.txz
